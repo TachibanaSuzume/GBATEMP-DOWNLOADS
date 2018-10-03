dfefb52a7a3bbf3988948f9e31bd0c39ae5fb352
mattytrog <41282276+mattytrog@users.noreply.github.com>
2018-10-03 13:33:14 +0100

    Updates. Updates everywhere

--- Chainloader/Trinket_Legacy_Method 3/main/main.ino
+++ Chainloader/Trinket_Legacy_Method 3/main/main.ino
@@ -1,231 +1,85 @@
 // Sam_fusee_launcher - Credits to quantum_cross for original code
-// Slightly altered by mattytrog for further experiments
+// Mattytrogs multi-payload. This is single payload version
 #include <Arduino.h>
 #include <Usb.h>
 #include <Adafruit_DotStar.h>
+#include <FlashStorage.h>
+
+//CHANGEABLE VALUES!!! CHANGE THE VALUES BELOW TO YOUR LIKING
+#define AMOUNT_OF_PAYLOADS 1         //set number of payloads required. 1 - 8. Leave unchanged for standard mode!!!
+#define AUTO_INCREASE_PAYLOAD_on 0   //Automatic increase payload when send fails. 1 = on, 0 = off
+#define AUTO_SEND_ON_PAYLOAD_INCREASE_PIN 0  // Automatic send when payload pin is activated. 1 = on, 0 = off
+#define FLASH_BEFORE_SEND_on 0  // flash payload number before attempting to send. 1 = on, 0 = off
+#define FLASH_AFTER_SEND_on 0   // flash payload number after send/attempted. Will show same payload number(if autoincrease is off, or next payload number) 1 = on, 0 = off
+#define LOOK_FOR_TEGRA_SECONDS 5       // how long to look for Tegra for & flash LED in search phase. Time in seconds
+#define LOOK_FOR_TEGRA_LED_SPEED 300 // how fast to blink when searching.
+#define DELAY_BEFORE_FLASH_WRITE_SECONDS 2 //get out of jail card. Press reset during this time and payload won`t be increased
+
+//set input/output pin numbers & times
+//#define PAYLOAD_INCREASE_PIN 1               //payload increase pin - touch to ground by default.
 #define WAKEUP_PIN_FALLING 2       // Method 2 pin to ground (power switch solder to switched side of 150R resistor)
 #define WAKEUP_PIN_RISING 4        // Method 3 pin to M92T36 pin 5 capacitor / rail
 #define JOYCON_STRAP_PIN 3            // Solder to pin 10 on joycon rail
-#define RCM_STRAP_TIME_us 1000000  // Amount of time to hold RCM_STRAP low and then launch payload
 #define VOLUP_STRAP_PIN 0         // Use with Method 3 only. With method 2, the trinket doesn`t boot fast enough. Bootloader needs modification
+
+//set LED on/off times
+#define PAYLOAD_FLASH_LED_ON_TIME_SECONDS 0.05 // controls blink during payload indication. On
+#define PAYLOAD_FLASH_LED_OFF_TIME_SECONDS 0.4 // as above, but amount of time for DARKNESS ;)
+#define STATUS_LED_TIME_us 1000000 // How long to show red or green light for success or fail - 1 second
+
+//set time to hold straps low for to enter RCM.
+#define RCM_STRAP_TIME_us 1000000  // Amount of time to hold RCM_STRAP low and then launch payload
 #define ONBOARD_LED 13
-#define LED_CONFIRM_TIME_us 2000000 // How long to show red or green light for success or fail - 2 seconds
 
-#include "hekateload.h"
+//includes
+#include "s2load.h"
+#include "usb_setup.h"
 
-#define INTERMEZZO_SIZE 92
-const byte intermezzo[INTERMEZZO_SIZE] =
-{
-  0x44, 0x00, 0x9F, 0xE5, 0x01, 0x11, 0xA0, 0xE3, 0x40, 0x20, 0x9F, 0xE5, 0x00, 0x20, 0x42, 0xE0,
-  0x08, 0x00, 0x00, 0xEB, 0x01, 0x01, 0xA0, 0xE3, 0x10, 0xFF, 0x2F, 0xE1, 0x00, 0x00, 0xA0, 0xE1,
-  0x2C, 0x00, 0x9F, 0xE5, 0x2C, 0x10, 0x9F, 0xE5, 0x02, 0x28, 0xA0, 0xE3, 0x01, 0x00, 0x00, 0xEB,
-  0x20, 0x00, 0x9F, 0xE5, 0x10, 0xFF, 0x2F, 0xE1, 0x04, 0x30, 0x90, 0xE4, 0x04, 0x30, 0x81, 0xE4,
-  0x04, 0x20, 0x52, 0xE2, 0xFB, 0xFF, 0xFF, 0x1A, 0x1E, 0xFF, 0x2F, 0xE1, 0x20, 0xF0, 0x01, 0x40,
-  0x5C, 0xF0, 0x01, 0x40, 0x00, 0x00, 0x02, 0x40, 0x00, 0x00, 0x01, 0x40,
-};
-
-#define PACKET_CHUNK_SIZE 0x1000
-
-#ifdef DEBUG
-#define DEBUG_PRINT(x)  Serial.print (x)
-#define DEBUG_PRINTLN(x)  Serial.println (x)
-#define DEBUG_PRINTHEX(x,y)  serialPrintHex (x,y)
-#else
-#define DEBUG_PRINT(x)
-#define DEBUG_PRINTLN(x)
-#define DEBUG_PRINTHEX(x,y)
-#endif
-
-USBHost usb;
-EpInfo epInfo[3];
-
-byte usbWriteBuffer[PACKET_CHUNK_SIZE] = {0};
-uint32_t usbWriteBufferUsed = 0;
-uint32_t packetsWritten = 0;
-
-bool foundTegra = false;
-byte tegraDeviceAddress = -1;
-
+FlashStorage(stored_payload, int);
 unsigned long lastCheckTime = 0;
-unsigned long defaultpayload = 1;
+int storedpayload = stored_payload.read();
+int autoincrease = AUTO_INCREASE_PAYLOAD_on;
+int newpayload = storedpayload;
+int startblink = 1;
+int currentblink;
 
-const char *hexChars = "0123456789ABCDEF";
-void serialPrintHex(const byte *data, byte length)
-{
-  for (int i = 0; i < length; i++)
-  {
-    DEBUG_PRINT(hexChars[(data[i] >> 4) & 0xF]);
-    DEBUG_PRINT(hexChars[data[i] & 0xF]);
-  }
-  DEBUG_PRINTLN();
+void dropstraps() {
+  pinMode(JOYCON_STRAP_PIN, OUTPUT);
+  pinMode(VOLUP_STRAP_PIN, OUTPUT);
+  pinMode(ONBOARD_LED, OUTPUT);
+  digitalWrite(JOYCON_STRAP_PIN, LOW);
+  digitalWrite(VOLUP_STRAP_PIN, LOW);
+  delayMicroseconds (RCM_STRAP_TIME_us);
 }
 
-Adafruit_DotStar strip = Adafruit_DotStar(1, INTERNAL_DS_DATA, INTERNAL_DS_CLK, DOTSTAR_BGR);
-
-void usbOutTransferChunk(uint32_t addr, uint32_t ep, uint32_t nbytes, uint8_t* data)
-{
-
-
-  EpInfo* epInfo = usb.getEpInfoEntry(addr, ep);
-
-  usb_pipe_table[epInfo->epAddr].HostDescBank[0].CTRL_PIPE.bit.PDADDR = addr;
-
-  if (epInfo->bmSndToggle)
-    USB->HOST.HostPipe[epInfo->epAddr].PSTATUSSET.reg = USB_HOST_PSTATUSSET_DTGL;
-  else
-    USB->HOST.HostPipe[epInfo->epAddr].PSTATUSCLR.reg = USB_HOST_PSTATUSCLR_DTGL;
-
-  UHD_Pipe_Write(epInfo->epAddr, PACKET_CHUNK_SIZE, data);
-  uint32_t rcode = usb.dispatchPkt(tokOUT, epInfo->epAddr, 15);
-  if (rcode)
-  {
-    if (rcode == USB_ERROR_DATATOGGLE)
-    {
-      epInfo->bmSndToggle = USB_HOST_DTGL(epInfo->epAddr);
-      if (epInfo->bmSndToggle)
-        USB->HOST.HostPipe[epInfo->epAddr].PSTATUSSET.reg = USB_HOST_PSTATUSSET_DTGL;
-      else
-        USB->HOST.HostPipe[epInfo->epAddr].PSTATUSCLR.reg = USB_HOST_PSTATUSCLR_DTGL;
-    }
-    else
-    {
-      strip.setPixelColor(0, 64, 0, 0); strip.show();
-      DEBUG_PRINTLN("Error in OUT transfer");
-      return;
-    }
-  }
-
-  epInfo->bmSndToggle = USB_HOST_DTGL(epInfo->epAddr);
+void normalstraps(){
+  pinMode(JOYCON_STRAP_PIN, INPUT);
+  pinMode(VOLUP_STRAP_PIN, INPUT);
+  pinMode(WAKEUP_PIN_RISING, INPUT);
+  pinMode(WAKEUP_PIN_FALLING, INPUT_PULLUP);
+  //pinMode(PAYLOAD_INCREASE_PIN, INPUT_PULLUP); 
 }
+void firstboot() {
 
-void usbFlushBuffer()
-{
-  usbOutTransferChunk(tegraDeviceAddress, 0x01, PACKET_CHUNK_SIZE, usbWriteBuffer);
-
-  memset(usbWriteBuffer, 0, PACKET_CHUNK_SIZE);
-  usbWriteBufferUsed = 0;
-  packetsWritten++;
-}
-
-// This accepts arbitrary sized USB writes and will automatically chunk them into writes of size 0x1000 and increment
-// packetsWritten every time a chunk is written out.
-void usbBufferedWrite(const byte *data, uint32_t length)
-{
-  while (usbWriteBufferUsed + length >= PACKET_CHUNK_SIZE)
-  {
-    uint32_t bytesToWrite = min(PACKET_CHUNK_SIZE - usbWriteBufferUsed, length);
-    memcpy(usbWriteBuffer + usbWriteBufferUsed, data, bytesToWrite);
-    usbWriteBufferUsed += bytesToWrite;
-    usbFlushBuffer();
-    data += bytesToWrite;
-    length -= bytesToWrite;
+  if (!newpayload) {
+    newpayload = 1;
+    storedpayload = 1;
   }
-
-  if (length > 0)
-  {
-    memcpy(usbWriteBuffer + usbWriteBufferUsed, data, length);
-    usbWriteBufferUsed += length;
+  if (!autoincrease) {
+    autoincrease = AUTO_INCREASE_PAYLOAD_on;
   }
 }
 
-void usbBufferedWriteU32(uint32_t data)
-{
-  usbBufferedWrite((byte *)&data, 4);
-}
-
-void readTegraDeviceID(byte *deviceID)
-{
-  byte readLength = 16;
-  UHD_Pipe_Alloc(tegraDeviceAddress, 0x01, USB_HOST_PTYPE_BULK, USB_EP_DIR_IN, 0x40, 0, USB_HOST_NB_BK_1);
-
-  if (usb.inTransfer(tegraDeviceAddress, 0x01, &readLength, deviceID))
-    DEBUG_PRINTLN("Failed to get device ID!");
-}
-
-void sendPayload(const byte *payload, uint32_t payloadLength)
-{
-  byte zeros[0x1000] = {0};
-
-  usbBufferedWriteU32(0x30298);
-  usbBufferedWrite(zeros, 680 - 4);
-
-  for (uint32_t i = 0; i < 0x3C00; i++)
-    usbBufferedWriteU32(0x4001F000);
-
-  usbBufferedWrite(intermezzo, INTERMEZZO_SIZE);
-  usbBufferedWrite(zeros, 0xFA4);
-  usbBufferedWrite(payload, payloadLength);
-  usbFlushBuffer();
-}
-
-void findTegraDevice(UsbDeviceDefinition *pdev)
-{
-  uint32_t address = pdev->address.devAddress;
-  USB_DEVICE_DESCRIPTOR deviceDescriptor;
-  if (usb.getDevDescr(address, 0, 0x12, (uint8_t *)&deviceDescriptor))
-  {
-    DEBUG_PRINTLN("Error getting device descriptor.");
-    return;
+void writetoflash(){
+  if (storedpayload != newpayload) {
+    stored_payload.write(newpayload);
+    setLedColor("white");
+    delayMicroseconds(STATUS_LED_TIME_us);
+    setLedColor("black");
   }
-
-  if (deviceDescriptor.idVendor == 0x0955 && deviceDescriptor.idProduct == 0x7321)
-  {
-    tegraDeviceAddress = address;
-    foundTegra = true;
-  }
-}
-
-void setupTegraDevice()
-{
-  epInfo[0].epAddr = 0;
-  epInfo[0].maxPktSize = 0x40;
-  epInfo[0].epAttribs = USB_TRANSFER_TYPE_CONTROL;
-  epInfo[0].bmNakPower = USB_NAK_MAX_POWER;
-  epInfo[0].bmSndToggle = 0;
-  epInfo[0].bmRcvToggle = 0;
-
-  epInfo[1].epAddr = 0x01;
-  epInfo[1].maxPktSize = 0x40;
-  epInfo[1].epAttribs = USB_TRANSFER_TYPE_BULK;
-  epInfo[1].bmNakPower = USB_NAK_MAX_POWER;
-  epInfo[1].bmSndToggle = 0;
-  epInfo[1].bmRcvToggle = 0;
-
-  usb.setEpInfoEntry(tegraDeviceAddress, 2, epInfo);
-  usb.setConf(tegraDeviceAddress, 0, 0);
-  usb.Task();
-
-  UHD_Pipe_Alloc(tegraDeviceAddress, 0x01, USB_HOST_PTYPE_BULK, USB_EP_DIR_IN, 0x40, 0, USB_HOST_NB_BK_1);
-}
-
-void sleep(int errorCode) {
-  // Turn off all LEDs and go to sleep. To launch another payload, press the reset button on the device.
-  //delay(100);
-  digitalWrite(PIN_LED_RXL, HIGH);
-  digitalWrite(PIN_LED_TXL, HIGH);
-  digitalWrite(ONBOARD_LED, LOW);
-  if (errorCode == 1) {
-    setLedColor("green"); //led to red
-    delayMicroseconds(LED_CONFIRM_TIME_us);
-    setLedColor("black"); //led to off
-  } else {
-    setLedColor("red"); //led to red
-    delayMicroseconds(LED_CONFIRM_TIME_us);
-    setLedColor("black"); //led to off
-  }
-
-  foundTegra = false;
-  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk; /* Enable deepsleep */
-
-  GCLK->CLKCTRL.reg = uint16_t(
-      GCLK_CLKCTRL_CLKEN |
-      GCLK_CLKCTRL_GEN_GCLK2 |
-      GCLK_CLKCTRL_ID( GCLK_CLKCTRL_ID_EIC_Val )
-  );
-  while (GCLK->STATUS.bit.SYNCBUSY) {}
-  
-  __DSB(); /* Ensure effect of last store takes effect */
-  __WFI(); /* Enter sleep mode */
+  storedpayload = newpayload;
+  newpayload = storedpayload;
+  return;
 }
 
 void setLedColor(const char color[]) {
@@ -247,47 +101,106 @@ void setLedColor(const char color[]) {
   strip.show();
 }
 
-void wakeup_rising(){
-  // Both straps Low
-  pinMode(JOYCON_STRAP_PIN, OUTPUT);
-  pinMode(VOLUP_STRAP_PIN, OUTPUT);
-  digitalWrite(JOYCON_STRAP_PIN, LOW);
-  digitalWrite(VOLUP_STRAP_PIN, LOW);
-  setLedColor("blue");
-  // 1 second
-  delayMicroseconds(RCM_STRAP_TIME_us);
+void setPayloadColor(int payloadcolornumber) {
+  if (payloadcolornumber == 1) {
+    strip.setPixelColor(0, 0, 128, 0);
+  } else if (payloadcolornumber == 2) {
+    strip.setPixelColor(0, 0, 0, 128);
+  } else if (payloadcolornumber == 3) {
+    strip.setPixelColor(0, 128, 128, 0);
+  } else if (payloadcolornumber == 4) {
+    strip.setPixelColor(0, 128, 0, 128);
+  } else if (payloadcolornumber == 5) {
+    strip.setPixelColor(0, 0, 128, 0);
+  } else if (payloadcolornumber == 6) {
+    strip.setPixelColor(0, 0, 0, 128);
+  } else if (payloadcolornumber == 7) {
+    strip.setPixelColor(0, 128, 128, 0);
+  } else if (payloadcolornumber == 8) {
+    strip.setPixelColor(0, 128, 0, 128);
+  }
+  strip.show();
+}
+
+void wakeup() { 
+  dropstraps();
   SCB->AIRCR = ((0x5FA << SCB_AIRCR_VECTKEY_Pos) | SCB_AIRCR_SYSRESETREQ_Msk); //full software reset
 }
 
-void wakeup_falling(){
-  // Both Straps Low
-  pinMode(JOYCON_STRAP_PIN, OUTPUT);
-  pinMode(VOLUP_STRAP_PIN, OUTPUT);
-  digitalWrite(JOYCON_STRAP_PIN, LOW);
-  digitalWrite(VOLUP_STRAP_PIN, LOW);
-  setLedColor("green");
-  // 1 second
-  delayMicroseconds(RCM_STRAP_TIME_us);
-  SCB->AIRCR = ((0x5FA << SCB_AIRCR_VECTKEY_Pos) | SCB_AIRCR_SYSRESETREQ_Msk); //full software reset
+void blink_led() {
+  for (currentblink = 0; currentblink < newpayload; ++currentblink) {
+    setLedColor("black");
+    digitalWrite(ONBOARD_LED, LOW);
+    delayMicroseconds(PAYLOAD_FLASH_LED_OFF_TIME_SECONDS * 1000000);
+    setPayloadColor(newpayload);
+    digitalWrite(ONBOARD_LED, HIGH);
+    delayMicroseconds(PAYLOAD_FLASH_LED_ON_TIME_SECONDS * 1000000);
+  }
+  setLedColor("black");
+  digitalWrite(ONBOARD_LED, LOW);
+  delayMicroseconds(PAYLOAD_FLASH_LED_OFF_TIME_SECONDS * 1000000);
+}  
+
+//choose and flash LED
+void increase_payload() {
+  ++newpayload;
+  if (newpayload > AMOUNT_OF_PAYLOADS) {
+    newpayload = 1;
+  }
+  if (FLASH_AFTER_SEND_on == 1) {
+    blink_led();
+  }
+  if (AUTO_SEND_ON_PAYLOAD_INCREASE_PIN == 1) {
+    writetoflash();
+    NVIC_SystemReset();
+  }
 }
 
-void setup()
-{
-  // This continues after the reset after a wakeup_rising or wakeup_falling
-  // Set straps as input to "stealth" any funny business on the RCM_STRAP
-  pinMode(JOYCON_STRAP_PIN, INPUT);
-  pinMode(VOLUP_STRAP_PIN, INPUT);
-  pinMode(WAKEUP_PIN_RISING, INPUT);
-  pinMode(WAKEUP_PIN_FALLING, INPUT_PULLUP);
+void increase_payload_automatic() {
+  if (AUTO_INCREASE_PAYLOAD_on == 1) { 
+  ++newpayload;
+  if (newpayload > AMOUNT_OF_PAYLOADS) {
+    newpayload = 1;
+    }
+  }
+  if (FLASH_AFTER_SEND_on == 1) {
+      blink_led();
+      writetoflash();
+    } else writetoflash();
+}
 
-  // Before sleeping, make sure that we can wake up again when the switch turns on
-  // by attaching interrupts to the wakeup pins
-  attachInterrupt(WAKEUP_PIN_RISING, wakeup_rising, RISING);
-  attachInterrupt(WAKEUP_PIN_FALLING, wakeup_falling, FALLING);
-  EIC->WAKEUP.vec.WAKEUPEN |= (1<<6);
+void decrease_payload() {
 
+}
+
+void sleep(int errorCode) {
+  // Turn off all LEDs and go to sleep. To launch another payload, press the reset button on the device.
+  //delay(100);
+  //digitalWrite(PIN_LED_RXL, HIGH);
+  //digitalWrite(PIN_LED_TXL, HIGH);
+  digitalWrite(ONBOARD_LED, LOW);
+  if (errorCode == 1) {
+    setLedColor("green");
+    delayMicroseconds(STATUS_LED_TIME_us);
+    setLedColor("black");
+  } else {
+    setLedColor("red");
+    delayMicroseconds(STATUS_LED_TIME_us);
+    setLedColor("black");
+    increase_payload_automatic(); //if autoincrease is enabled, payload counter will increase just before entering standby
+    }
+  standby();
+  }
+
+void setinterrupts() {
+  attachInterrupt(WAKEUP_PIN_RISING, wakeup, RISING);
+  attachInterrupt(WAKEUP_PIN_FALLING, wakeup, FALLING);
+  //attachInterrupt(PAYLOAD_INCREASE_PIN, increase_payload, FALLING);
+  EIC->WAKEUP.vec.WAKEUPEN |= (1 << 6);
+}
+
+void lookfortegra() {
   strip.begin();
-
   int usbInitialized = usb.Init();
 #ifdef DEBUG
   Serial.begin(115200);
@@ -295,31 +208,39 @@ void setup()
 #endif
 
   if (usbInitialized == -1) sleep(-1);
-
   DEBUG_PRINTLN("Ready! Waiting for Tegra...");
-  bool blink = true;
+  if (FLASH_BEFORE_SEND_on == 1) {
+    blink_led();
+  }
+  
   int currentTime = 0;
+  bool blink = true;
   while (!foundTegra)
   {
-    currentTime = millis();
     usb.Task();
-
-    if (currentTime > lastCheckTime + 100) {
+    ++currentTime;
+    delay(1);
+    if ((currentTime) > lastCheckTime + LOOK_FOR_TEGRA_LED_SPEED) {
       usb.ForEachUsbDevice(&findTegraDevice);
       if (blink && !foundTegra) {
-        setLedColor("white"); //led to white
+        setPayloadColor(newpayload);
+        digitalWrite(ONBOARD_LED, HIGH);
       } else {
-        setLedColor("black"); //led to black
+        setLedColor("black");
+        digitalWrite(ONBOARD_LED, LOW);
       }
       blink = !blink;
       lastCheckTime = currentTime;
     }
-    if (currentTime > 1500) {
+    if (currentTime > (LOOK_FOR_TEGRA_SECONDS * 1000)) {
+      writetoflash();
       sleep(-1);
     }
-
   }
+  pushpayload();
+}
 
+void pushpayload() {
   DEBUG_PRINTLN("Found Tegra!");
   setupTegraDevice();
 
@@ -331,8 +252,23 @@ void setup()
   DEBUG_PRINTLN("Sending payload...");
   UHD_Pipe_Alloc(tegraDeviceAddress, 0x01, USB_HOST_PTYPE_BULK, USB_EP_DIR_OUT, 0x40, 0, USB_HOST_NB_BK_1);
   packetsWritten = 0;
-  sendPayload(fuseeBin, FUSEE_BIN_SIZE);
-
+  if (newpayload == 1) {
+    sendPayload(fuseeBin, FUSEE_BIN_SIZE); //sendPayload(fuseeBin1, FUSEE_BIN_SIZE1);
+  } //else if (newpayload == 2) {
+    //sendPayload(fuseeBin2, FUSEE_BIN_SIZE2);
+  //} else if (newpayload == 3) {
+    //sendPayload(fuseeBin3, FUSEE_BIN_SIZE3);
+  //} else if (newpayload == 4) {
+    //sendPayload(fuseeBin4, FUSEE_BIN_SIZE4);
+  //} else if (newpayload == 5) {
+    //sendPayload(fuseeBin5, FUSEE_BIN_SIZE5);
+  //} else if (newpayload == 6) {
+    //sendPayload(fuseeBin6, FUSEE_BIN_SIZE6);
+  //} else if (newpayload == 7) {
+    //sendPayload(fuseeBin7, FUSEE_BIN_SIZE7);
+  //} else if (newpayload == 8) {
+    //sendPayload(fuseeBin8, FUSEE_BIN_SIZE8);
+  //}
   if (packetsWritten % 2 != 1)
   {
     DEBUG_PRINTLN("Switching to higher buffer...");
@@ -343,11 +279,22 @@ void setup()
   usb.ctrlReq(tegraDeviceAddress, 0, USB_SETUP_DEVICE_TO_HOST | USB_SETUP_TYPE_STANDARD | USB_SETUP_RECIPIENT_INTERFACE,
               0x00, 0x00, 0x00, 0x00, 0x7000, 0x7000, usbWriteBuffer, NULL);
   DEBUG_PRINTLN("Done!");
-
+  if (FLASH_AFTER_SEND_on == 1) {
+  blink_led();// flash payload number on LED to show which payload was sent.
+  }
   sleep(1);
-
-
 }
+
+void setup()
+{
+  usb.Task(); //host mode
+  //dropstraps(); //pull straps low
+  normalstraps(); //stop them misbehaving
+  firstboot(); //get flash memory status. If invalid, make valid.
+  setinterrupts();
+  lookfortegra();
+}
+
 
 void loop()
 {
